0f0632bbab39cacdba70c3fcf4e8a9beb072ed53
nlpss99019 <nlpss99019@gmail.com>
2024-03-06 17:36:31 +0800

    第一次commit 第二次commit(修改文字元件) 第三次commit(修改文字元件) 第四次commit(修改文字元件)

--- app/src/main/java/tw/edu/pu/csim/s1114633/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114633/lotto/MainActivity.kt
@@ -12,4 +12,11 @@ class MainActivity : AppCompatActivity() {
 
         txv.text = "0"
     }
+    fun happy(v: View){
+
+        var txv: TextView = findViewById(R.id.txv)
+
+        txv.text = (1..100).random().toString()
+
+    }
 }
